25321bb314f57c08bb8bdd6c281aa31ec8301099
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-01-17 14:51:05 +0100

    Closes #5

--- back/struct.go
+++ back/struct.go
@@ -1,68 +1,13 @@
 package back
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"strings"
-)
-
-// Fonction pour modifié le JSON
-func EditJSON(ModifiedArticle []Personnage) {
-
-	modifiedJSON, errMarshal := json.Marshal(ModifiedArticle)
-	if errMarshal != nil {
-		fmt.Println("Error encodage ", errMarshal.Error())
-		return
-	}
-
-	// Écrire le JSON modifié dans le fichier
-	err := os.WriteFile("JSON/bdd.json", modifiedJSON, 0644)
-	if err != nil {
-		fmt.Println("Erreur lors de l'écriture du fichier JSON modifié:", err)
-		return
-	}
-
+type Personnage struct {
+	Id      int    `json:"id"`
+	Genders string   `json:"genders"`
+	Shirt   int    `json:"shirt"`
+	Pants   int    `json:"pants"`
+	Name    string `json:"name"`
 }
 
-// Fonction pour mettre le JSON dans une struct
-func ReadJSON() ([]Personnage, error) {
-	jsonFile, err := os.ReadFile("JSON/bdd.json")
-	if err != nil {
-		fmt.Println("Error reading", err.Error())
-	}
-
-	var jsonData []Personnage
-	err = json.Unmarshal(jsonFile, &jsonData)
-	return jsonData, err
-}
-
-// Fonction pour savoir si l'id existe déjà
-func IdAlreadyExists(nb int) bool {
-	for i := 0; i < len(Persons); i++ {
-		if Persons[i].Id == nb {
-			return true
-		}
-	}
-	return false
-}
-
-// Fonction pour générer un Id disponible
-func GenerateID() int {
-	if !IdAlreadyExists(len(Persons) + 1) {
-		return len(Persons) + 1
-	} else {
-		t := LstIDSuppr[0]
-		if len(LstIDSuppr) > 1 {
-			LstIDSuppr = LstIDSuppr[1:]
-		} else {
-			LstIDSuppr = []int{}
-		}
-		return t
-	}
-}
-
-// Fonction pour rechercher
-func Search(word string, s string) bool {
-	return strings.Contains(strings.ToLower(word), strings.ToLower(s))
-}
+var Person Personnage
+var Persons []Personnage
+var LstIDSuppr []int
